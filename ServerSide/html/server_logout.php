f60b8ccab94c9d459ad90fded34440994f594303
Kobe Angelo Rosales <2221513@slu.edu.ph>
2023-12-11 23:59:23 +0800

    aaaa

--- ServerSide/html/server_logout.php
+++ ServerSide/html/server_logout.php
@@ -8,6 +8,6 @@ $_SESSION = array();
 session_destroy();
 
 // Redirect to the login page
-header("Location: /web-systems-development/Index/index.html");
+header("Location: /web-systems-development/ServerSide/html/server_index.php");
 exit;
 ?>
