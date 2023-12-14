6a93ae0da8f1d80b783ff30211f053c94731687b
Kobe Angelo Rosales <2221513@slu.edu.ph>
2023-12-14 14:14:43 +0800

    sql push

--- teampogi.sql
+++ teampogi.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3306
--- Generation Time: Dec 13, 2023 at 08:47 PM
+-- Generation Time: Dec 14, 2023 at 06:14 AM
 -- Server version: 8.0.31
 -- PHP Version: 8.0.26
 
@@ -114,7 +114,7 @@ CREATE TABLE IF NOT EXISTS `feedback` (
   `feedback_date` date NOT NULL,
   PRIMARY KEY (`feedback_id`),
   KEY `record_id` (`record_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=15 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=39 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `feedback`
@@ -124,7 +124,9 @@ INSERT INTO `feedback` (`feedback_id`, `record_id`, `feedback_text`, `feedback_d
 (1, 1, 'The intern completed his work and performed well in regards to the tasks that is given to him.', '2023-09-05'),
 (2, 2, 'The intern is currently performing well. She complies to the given tasks and do it on time.', '2023-11-21'),
 (12, 1, 'Very Bad!', '2023-12-11'),
-(14, 1, 'Very Good!', '2023-12-13');
+(14, 1, 'Very Good!', '2023-12-13'),
+(15, 3, 'Tuco, you are so bad!!', '2023-12-13'),
+(20, 5, 'Jane, you did a great job with Jesse. Keep it up with that Meath!', '2023-12-14');
 
 -- --------------------------------------------------------
 
@@ -146,7 +148,7 @@ CREATE TABLE IF NOT EXISTS `interndetails` (
   PRIMARY KEY (`intern_id`),
   KEY `user_id` (`user_id`),
   KEY `adviser_id` (`adviser_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=19 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=21 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `interndetails`
@@ -162,7 +164,9 @@ INSERT INTO `interndetails` (`intern_id`, `user_id`, `adviser_id`, `firstName`, 
 (10, 10, 2, 'Skinny', 'Pete', 'spete@slu.edu.ph', 'Baguio City', 'Saint Louis University', 'Computer Science Intern Student'),
 (11, 11, 2, 'Juan', 'Bolsa', 'jbolsa@slu.edu.ph', 'Bakakeng Sur, Baguio City', 'Saint Louis University', 'Computer Science Intern Student'),
 (12, 12, 2, 'Chuck', 'McGill', 'cmcgill@slu.edu.ph', 'Bakakeng Sur, Baguio City', 'Saint Louis University', 'Computer Science Intern Student'),
-(13, 13, 2, 'Kim', 'Wexler', 'kwexler@slu.edu.ph', 'Bakakeng Norte, Baguio City', 'Saint Louis University', 'Computer Science Intern Student');
+(13, 13, 2, 'Kim', 'Wexler', 'kwexler@slu.edu.ph', 'Bakakeng Norte, Baguio City', 'Saint Louis University', 'Computer Science Intern Student'),
+(19, 21, 2, 'Skyler', 'White', 'swhite@slu.edu.ph', 'Zambales', 'Saint Louis University', 'Computer Science Intern Student'),
+(20, 22, 2, 'Hector', 'Salamanca', 'hsalamanca@slu.edu.ph', 'Olongapo City', 'Saint Louis University', 'Computer Science Intern Student');
 
 -- --------------------------------------------------------
 
@@ -187,7 +191,7 @@ CREATE TABLE IF NOT EXISTS `internshiprecords` (
   KEY `adviser_id` (`adviser_id`),
   KEY `program_id` (`program_id`),
   KEY `administrator_id` (`administrator_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=45 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `internshiprecords`
@@ -195,15 +199,17 @@ CREATE TABLE IF NOT EXISTS `internshiprecords` (
 
 INSERT INTO `internshiprecords` (`record_id`, `intern_id`, `adviser_id`, `program_id`, `administrator_id`, `hours_completed`, `hours_remaining`, `start_date`, `end_date`, `record_status`) VALUES
 (1, 4, 2, 36, 2, 100, 0, '2023-07-01', '2023-08-31', 'Completed'),
-(2, 5, 2, 37, 2, 83, 17, '2023-11-01', '2023-12-01', 'In Progress'),
-(3, 6, 2, 39, 2, 50, 50, '2023-10-31', '2023-11-30', 'In Progress'),
+(2, 5, 2, 37, 2, 95, 5, '2023-11-01', '2023-12-01', 'In Progress'),
+(3, 6, 2, 39, 2, 70, 30, '2023-10-31', '2023-11-30', 'In Progress'),
 (4, 7, 2, 38, 2, 0, 100, '2023-10-31', '2023-11-30', 'In Progress'),
 (5, 8, 2, 38, 2, 0, 100, '2023-10-31', '2023-11-30', 'In Progress'),
 (6, 9, 2, 38, 2, 0, 100, '2023-10-31', '2023-11-30', 'In Progress'),
 (8, 10, 2, 38, 2, 0, 100, '2023-10-31', '2023-11-30', 'In Progress'),
 (9, 11, 2, 38, 2, 0, 100, '2023-10-31', '2023-11-30', 'In Progress'),
 (10, 12, 2, 38, 2, 0, 100, '2023-10-31', '2023-11-30', 'In Progress'),
-(11, 13, 2, 38, 2, 0, 100, '2023-10-31', '2023-11-30', 'In Progress');
+(11, 13, 2, 38, 2, 0, 100, '2023-10-31', '2023-11-30', 'In Progress'),
+(12, 19, 2, 39, 2, 0, 100, '2023-10-31', '2023-11-30', 'In Progress'),
+(44, 20, 2, NULL, 2, 0, 100, '0000-00-00', '2023-12-30', 'In Progress');
 
 -- --------------------------------------------------------
 
@@ -220,7 +226,7 @@ CREATE TABLE IF NOT EXISTS `ojtprograms` (
   `end_date` date NOT NULL,
   PRIMARY KEY (`program_id`),
   KEY `administrator_id` (`administrator_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=40 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=46 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `ojtprograms`
@@ -248,7 +254,7 @@ CREATE TABLE IF NOT EXISTS `timetrack` (
   `hours_rendered` decimal(5,2) NOT NULL,
   PRIMARY KEY (`timetrack_id`),
   KEY `record_id` (`record_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=17 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `timetrack`
@@ -256,7 +262,14 @@ CREATE TABLE IF NOT EXISTS `timetrack` (
 
 INSERT INTO `timetrack` (`timetrack_id`, `record_id`, `date`, `timein`, `timeout`, `hours_rendered`) VALUES
 (4, 2, '2023-12-14', '07:30:00', '15:30:00', '8.00'),
-(5, 2, '2023-12-14', '08:00:00', '13:00:00', '5.00');
+(5, 2, '2023-12-15', '08:00:00', '13:00:00', '5.00'),
+(7, 2, '2023-12-16', '08:53:00', '13:53:00', '5.00'),
+(8, 2, '2023-12-17', '08:53:00', '17:55:00', '9.03'),
+(9, 2, '2023-12-19', '12:53:00', '18:08:00', '5.25'),
+(11, 2, '2023-12-20', '09:05:00', '11:05:00', '2.00'),
+(14, 2, '2023-12-28', '11:29:00', '13:29:00', '2.00'),
+(15, 2, '2023-12-27', '09:44:00', '14:44:00', '5.00'),
+(16, 2, '2023-12-28', '09:48:00', '17:50:00', '8.03');
 
 -- --------------------------------------------------------
 
@@ -272,7 +285,7 @@ CREATE TABLE IF NOT EXISTS `users` (
   `user_type` enum('Intern','Adviser','Administrator') COLLATE utf8mb4_general_ci NOT NULL,
   PRIMARY KEY (`user_id`),
   UNIQUE KEY `username` (`username`)
-) ENGINE=InnoDB AUTO_INCREMENT=21 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=23 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `users`
@@ -290,7 +303,9 @@ INSERT INTO `users` (`user_id`, `username`, `password`, `user_type`) VALUES
 (12, 'intern9', '$2b$10$WiuWR0hzSGP15pcu1/5AG.cbs93rGaZxM7AEpOHdMbbI5vIX8/yk6', 'Intern'),
 (13, 'intern10', '$2b$10$5yy0Aro5A4RqxxqHaZR2Q.5VRDx6JL5sZDpBH3970S1PaA4JscRxK', 'Intern'),
 (14, 'adviser1', '$2b$10$pgI5Kl3fYGmY.EWffNfvpegncG82YzW.ZY6zJVrFjgGNrhODz2ASu', 'Adviser'),
-(15, 'admin1', '$2y$10$G2UQJaBZ5hSSejbA9DUWVe6QgifCRQrY/oEE6T7M9hnmEyvEFgKn2', 'Administrator');
+(15, 'admin1', '$2y$10$G2UQJaBZ5hSSejbA9DUWVe6QgifCRQrY/oEE6T7M9hnmEyvEFgKn2', 'Administrator'),
+(21, 'intern11', '$2y$10$Ywa0iev0VdQpw3oi3pxxbOhhl6rHO6tZqPHxC0xDBI8rw1Kr3dKqC', 'Intern'),
+(22, 'intern12', '$2y$10$Y/bAWiiibpOWWFVC3poE0.yfWyEYFuulEkXmfsFPYcHUlFDR8OAia', 'Intern');
 
 --
 -- Constraints for dumped tables
